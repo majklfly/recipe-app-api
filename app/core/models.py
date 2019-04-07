a487d225b0ef9ab90803353edbfec6f9bdf87076
Michal <majklfly@gmail.com>
2019-04-07 16:02:23 +0100

    added list recipe feature

--- app/core/models.py
+++ app/core/models.py
@@ -49,6 +49,7 @@ class Tag(models.Model):
     def __str__(self):
         return self.name
 
+
 class Ingredient(models.Model):
     """Ingredient to be used in a recipe"""
     name = models.CharField(max_length=255)
@@ -63,7 +64,7 @@ class Ingredient(models.Model):
 
 class Recipe(models.Model):
     """Recipe object"""
-    user=models.ForeignKey(
+    user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
     )
